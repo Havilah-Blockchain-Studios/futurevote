3f99e8112540fc4a8a963233250e56798d05873d
Derrick Koko <kokoderrick3@gmail.com>
2024-06-14 21:20:08 +0100

    "Updated User model in users app, added phone_number and address fields, and modified nin field to have max length. Also, updated settings.pyc file."

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,13 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 # Create your models here.
 class User(AbstractUser):
     email = models.EmailField(unique=True)
     username = models.CharField(max_length=100, unique=True)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    nin = models.CharField()
+    nin = models.CharField(max_length=100)
+    phone_number = models.CharField(max=100)
+    address = models.TextField(blank=True, null=True)
     date_joined = models.DateTimeField(auto_now_add=True)
     last_login = models.DateTimeField(auto_now=True)
     is_active = models.BooleanField(default=False)
